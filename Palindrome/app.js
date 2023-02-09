a9b76fad37dfd15cd96a6925bea20e300101b14a
ThirdMistrust <thirdmistrust@proton.me>
2023-02-09 20:34:26 +0100

    add comments

--- Palindrome/app.js
+++ Palindrome/app.js
@@ -1,9 +1,11 @@
 let user_input;
+// selection of elements
 let form = document.querySelector('#form')
 let input = document.querySelector('#user-input')
 let error = document.querySelector('#error')
 error.style.display = 'none'
 
+// function that compares the user input and its inverse 
 const check_palindrome = str => {
   let result = document.createElement('div')
   document.body.append(result)
@@ -17,12 +19,14 @@ const check_palindrome = str => {
   }
 }
 
+// capture user input to send it to the check_palindrome function
 form.addEventListener('submit', (e) => {
   e.preventDefault()
   user_input = input.value
   check_palindrome(user_input)
 })
 
+// error handling 
 input.addEventListener('keyup', () => {
   if (isNaN(input.value) || '') {
     error.style.display = 'none'
