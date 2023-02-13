6bcfa14894b88f9b8d74c36f8bfca98a96a39cc6
ThirdMistrust <thirdmistrust@proton.me>
2023-02-13 23:34:37 +0100

    add basic css

--- Palindrome/app.js
+++ Palindrome/app.js
@@ -3,15 +3,17 @@ let user_input;
 let form = document.querySelector('#form')
 let input = document.querySelector('#user-input')
 let error = document.querySelector('#error')
+let result = document.createElement('div')
 error.style.display = 'none'
 
 // function that compares the user input and its inverse 
 const check_palindrome = str => {
-  let result = document.createElement('div')
-  document.body.append(result)
+  result.remove()
+  form.appendChild(result)
+  result.className = 'result'
   let strChecked = str.split('').reverse().join('')
-  
   if (str === strChecked) {
+    document.remo
     return result.textContent = `${str} is a palindrome`
   }
   else {
@@ -24,6 +26,7 @@ form.addEventListener('submit', (e) => {
   e.preventDefault()
   user_input = input.value
   check_palindrome(user_input)
+  input.value = ''
 })
 
 // error handling 
